4a19c44fc1b88e64746d01be56c458c79d540f1a
Fury <ashiqasraf07@gmail.com>
2021-12-11 21:17:01 +0530

    Created posts components

---
+++ src/components/posts/Posts.jsx
@@ -0,0 +1,17 @@
+import Post from '../post/Post'
+import './posts.css'
+
+const Posts = () => {
+	return (
+		<div className="posts">
+			<Post />
+			<Post />
+			<Post />
+			<Post />
+			<Post />
+			<Post />
+		</div>
+	)
+}
+
+export default Posts
