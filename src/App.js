37826c882b81a5349b857e8e114329a1b61b58d6
Fury <ashiqasraf07@gmail.com>
2021-12-11 21:19:56 +0530

    Added App.js file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import Topbar from './components/topbar/Topbar'
+import Home from './pages/home/Home'
+import Single from './pages/single/Single'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+	return (
+		<div>
+			<Topbar />
+			{/* <Home /> */}
+			<Single />
+		</div>
+	)
 }
 
-export default App;
+export default App
