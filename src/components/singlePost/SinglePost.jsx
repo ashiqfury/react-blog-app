018f916caa93c202a9238d1bf60547910e5e8342
Fury <ashiqasraf07@gmail.com>
2021-12-20 22:09:56 +0530

    Completed single post page

--- src/components/singlePost/SinglePost.jsx
+++ src/components/singlePost/SinglePost.jsx
@@ -12,10 +12,32 @@ const SinglePost = () => {
 				<h1 className="singlePostTitle">
 					This is the title of the blog
 					<div className="singlePostEdit">
-						<i className="far fa-edit"></i>
-						<i className="far fa-trash-alt"></i>
+						<i className="singlePostIcon far fa-edit"></i>
+						<i className="singlePostIcon far fa-trash-alt"></i>
 					</div>
 				</h1>
+				<div className="singlePostInfo">
+					<span className="singlePostAuthor">
+						Author: <strong>Fury</strong>
+					</span>
+					<span className="singlePostDate">1 hour ago</span>
+				</div>
+				<p className="singlePostDesc">
+					Lorem, ipsum dolor sit amet consectetur adipisicing elit. In, temporibus consequatur
+					assumenda dolore asperiores inventore exercitationem quo doloribus, at, veniam quia minima
+					distinctio nobis quae optio eligendi? Quisquam dolores et reiciendis doloremque dolorem
+					dicta recusandae odit voluptatem voluptas officia! Temporibus impedit iusto ad ratione
+					molestiae, tempore voluptates magni corrupti libero ullam, tempora debitis? Tempore
+					recusandae facilis tenetur illo deserunt quos doloribus, totam harum id omnis porro
+					voluptate cum corrupti! Temporibus tempora non quas laborum. Quis nobis modi perferendis
+					voluptatibus illo voluptatum accusamus sint similique reprehenderit ullam totam sed
+					consequuntur, porro quibusdam quod sit ipsum fugit culpa hic! Asperiores, enim aut? Lorem,
+					ipsum dolor sit amet consectetur adipisicing elit. Non, earum sit numquam molestias
+					provident iusto officia dolorem doloremque accusantium sed. Quasi repudiandae odit sunt
+					dolorem omnis tenetur velit? Est dolores quam laudantium fugit provident facilis illum
+					libero eos voluptate quasi! Error id neque blanditiis unde dolores quasi quaerat sunt
+					aspernatur?
+				</p>
 			</div>
 		</div>
 	)
