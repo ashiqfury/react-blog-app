5998eb9a055efd26f46ebd0ea45191d685f5aee1
Fury <ashiqasraf07@gmail.com>
2021-12-21 19:38:42 +0530

    Completed register page

--- src/App.js
+++ src/App.js
@@ -1,20 +1,37 @@
 import Topbar from './components/topbar/Topbar'
 import Home from './pages/home/Home'
 import Login from './pages/login/Login'
+import Register from './pages/register/Register'
 import Settings from './pages/settings/Settings'
 import Single from './pages/single/Single'
 import Write from './pages/write/Write'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 const App = () => {
 	return (
-		<div>
+		<Router>
 			<Topbar />
-			{/* <Home /> */}
-			{/* <Single /> */}
-			{/* <Write /> */}
-			{/* <Settings /> */}
-			<Login />
-		</div>
+			<Switch>
+				<Route path="/" exact>
+					<Home />
+				</Route>
+				<Route path="/login">
+					<Login />
+				</Route>
+				<Route path="/register">
+					<Register />
+				</Route>
+				<Route path="/post/:postId">
+					<Single />
+				</Route>
+				<Route path="/write">
+					<Write />
+				</Route>
+				<Route path="/settings">
+					<Settings />
+				</Route>
+			</Switch>
+		</Router>
 	)
 }
 
---
+++ src/pages/register/Register.jsx
@@ -0,0 +1,21 @@
+import './register.css'
+
+const Register = () => {
+	return (
+		<div className="register">
+			<span className="registerTitle">Register</span>
+			<form className="registerForm">
+				<label>Username</label>
+				<input className="registerInput" type="text" placeholder="Enter your username..." />
+				<label>Email</label>
+				<input className="registerInput" type="text" placeholder="Enter your email..." />
+				<label>Password</label>
+				<input className="registerInput" type="password" placeholder="Enter your password..." />
+				<button className="registerButton">Register</button>
+				<button className="registerLoginButton">Login</button>
+			</form>
+		</div>
+	)
+}
+
+export default Register
