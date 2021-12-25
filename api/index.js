655e14bf3aef7ff3b19b0a9e262a0dfea4a9cc63
Fury <ashiqasraf07@gmail.com>
2021-12-25 22:22:58 +0530

    Completed posts routes

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ const dotenv = require('dotenv')
 const mongoose = require('mongoose')
 const authRoute = require('./routes/auth')
 const usersRoute = require('./routes/users')
+const postsRoute = require('./routes/posts')
 
 dotenv.config()
 
@@ -20,6 +21,7 @@ mongoose
 
 app.use('/api/auth', authRoute)
 app.use('/api/users', usersRoute)
+app.use('/api/posts', postsRoute)
 
 const port = 2506
 app.listen(port, () => {
