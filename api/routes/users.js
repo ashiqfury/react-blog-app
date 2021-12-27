09742704e93510169aa03cc1ed431f130f0316c0
Fury <ashiqasraf07@gmail.com>
2021-12-27 09:48:00 +0530

    Added get all users route

--- api/routes/users.js
+++ api/routes/users.js
@@ -58,4 +58,14 @@ router.get('/:id', async (req, res) => {
 	}
 })
 
+// GET ALL USER
+router.get('/', async (req, res) => {
+	try {
+		const users = await User.find()
+		res.status(200).json(users)
+	} catch (err) {
+		res.status(500).json(err)
+	}
+})
+
 module.exports = router
