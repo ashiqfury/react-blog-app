cb6af375f493e517e1d6dbd5ed13c6b8492e0fba
Fury <ashiqasraf07@gmail.com>
2021-12-27 19:20:15 +0530

    Added upload image routes

--- api/index.js
+++ api/index.js
@@ -5,6 +5,8 @@ const mongoose = require('mongoose')
 const authRoute = require('./routes/auth')
 const usersRoute = require('./routes/users')
 const postsRoute = require('./routes/posts')
+const categoriesRoute = require('./routes/categories')
+const multer = require('multer')
 
 dotenv.config()
 
@@ -19,9 +21,24 @@ mongoose
 	.then(() => console.log('Database is connected successfully...👍'))
 	.catch(err => console.log('Connection Failed..😢 ', err))
 
+const storage = multer.diskStorage({
+	destination: (req, file, callback) => {
+		callback(null, 'images')
+	},
+	filename: (req, file, callback) => {
+		callback(null, req.body.name) // hello.jpg
+	},
+})
+
+const upload = multer({ storage: storage })
+app.post('/api/upload', upload.single('file'), (req, res) => {
+	res.status(200).json('File has been uploaded')
+})
+
 app.use('/api/auth', authRoute)
 app.use('/api/users', usersRoute)
 app.use('/api/posts', postsRoute)
+app.use('/api/categories', categoriesRoute)
 
 const port = 2506
 app.listen(port, () => {
