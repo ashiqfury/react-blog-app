2a4dc7859054e8a2a18a6d8c653c60c031018ea0
Fury <ashiqasraf07@gmail.com>
2021-12-28 19:59:35 +0530

    Integrated register process

--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -1,24 +1,64 @@
+import axios from 'axios'
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
 import './register.css'
 
 const Register = () => {
+	const [username, setUsername] = useState('')
+	const [email, setEmail] = useState('')
+	const [password, setPassword] = useState('')
+	const [error, setError] = useState(false)
+
+	const handleSubmit = async e => {
+		e.preventDefault()
+		setError(false)
+		try {
+			const res = await axios.post('/auth/register', {
+				username,
+				email,
+				password,
+			})
+			res.data && window.location.replace('/login')
+		} catch (err) {
+			setError(true)
+		}
+	}
+
 	return (
 		<div className="register">
 			<span className="registerTitle">Register</span>
-			<form className="registerForm">
+			<form className="registerForm" onSubmit={handleSubmit}>
 				<label>Username</label>
-				<input className="registerInput" type="text" placeholder="Enter your username..." />
+				<input
+					className="registerInput"
+					type="text"
+					placeholder="Enter your username..."
+					onChange={e => setUsername(e.target.value)}
+				/>
 				<label>Email</label>
-				<input className="registerInput" type="text" placeholder="Enter your email..." />
+				<input
+					className="registerInput"
+					type="text"
+					placeholder="Enter your email..."
+					onChange={e => setEmail(e.target.value)}
+				/>
 				<label>Password</label>
-				<input className="registerInput" type="password" placeholder="Enter your password..." />
+				<input
+					className="registerInput"
+					type="password"
+					placeholder="Enter your password..."
+					onChange={e => setPassword(e.target.value)}
+				/>
 				<button className="registerButton">Register</button>
 
-				<button className="registerLoginButton">
+				<button className="registerLoginButton" type="submit">
 					<Link className="link" to="/login">
 						Login
 					</Link>
 				</button>
+				{error && (
+					<span style={{ color: 'crimson', marginTop: '10px' }}>Something went wrong!</span>
+				)}
 			</form>
 		</div>
 	)
