caf126dda033256a1c34e7ff837eee92fae82bda
Fury <ashiqasraf07@gmail.com>
2021-12-30 12:44:35 +0530

    Customized topbar and some components

--- client/src/components/Topbar.jsx
+++ client/src/components/Topbar.jsx
@@ -12,66 +12,75 @@ const Topbar = () => {
 
 	return (
 		<div className="topbar">
-			<div className="topLeft">
-				<i className="topIcon fab fa-facebook-square"></i>
-				<i className="topIcon fab fa-twitter-square"></i>
-				<i className="topIcon fab fa-pinterest-square"></i>
-				<i className="topIcon fab fa-instagram-square"></i>
+			<div className="topbar__left">
+				<Link to="/" className="link">
+					<span className="topbar__logo">Fury Blogz.</span>
+				</Link>
+				{/* <i className="topbar__icon fab fa-facebook-square"></i>
+				<i className="topbar__icon fab fa-twitter-square"></i>
+				<i className="topbar__icon fab fa-pinterest-square"></i>
+				<i className="topbar__icon fab fa-instagram-square"></i> */}
 			</div>
-			<div className="topCenter">
-				<ul className="topList">
-					<li className="topListItem">
+			<div className="topbar__center">
+				<ul className="topbar__list">
+					<li className="topbar__list--item">
 						<Link to="/" className="link">
 							HOME
 						</Link>
 					</li>
-					<li className="topListItem">
-						<Link to="/" className="link">
-							ABOUT
-						</Link>
-					</li>
-					<li className="topListItem">
-						<Link to="/" className="link">
-							CONTACT
-						</Link>
-					</li>
-					<li className="topListItem">
+					<li className="topbar__list--item">
 						<Link to="/write" className="link">
 							WRITE
 						</Link>
 					</li>
-					<li className="topListItem" onClick={handleLogout}>
+					<li className="topbar__list--item">
+						<Link to="/" className="link">
+							CONTACT
+						</Link>
+					</li>
+					<li className="topbar__list--item">
+						<Link to="/" className="link">
+							ABOUT
+						</Link>
+					</li>
+					<li className="topbar__list--item" onClick={handleLogout}>
 						{user && 'LOGOUT'}
 					</li>
 				</ul>
 			</div>
-			<div className="topRight">
+			<div className="topbar__right">
 				{user ? (
 					<>
 						<Link to="/settings" className="link">
 							{user.profilePic ? (
-								<img className="topImg" src={PF + user.profilePic} alt="" />
+								<img className="topbar__img" src={PF + user.profilePic} alt="" />
 							) : (
-								<img className="topImg" src={'http://localhost:2506/images/avatar.jpg'} alt="" />
+								<img
+									className="topbar__img"
+									src={'http://localhost:2506/images/avatar.jpg'}
+									alt=""
+								/>
 							)}
 						</Link>
-						<span className="topUsername">{user.username}</span>
+						<Link to="/settings" className="link">
+							<span className="topbar__username">{user.username}</span>
+						</Link>
 					</>
 				) : (
-					<ul className="topList">
-						<li className="topListItem">
+					<ul className="topbar__list">
+						<li className="topbar__list--item">
 							<Link className="link" to="/login">
 								LOGIN
 							</Link>
 						</li>
-						<li className="topListItem">
+						<li className="topbar__list--item">
 							<Link className="link" to="/register">
 								REGISTER
 							</Link>
 						</li>
 					</ul>
 				)}
-				<i className="topSearchIcon fas fa-search"></i>
+				<i className="topbar__icon--search fas fa-search"></i>
 			</div>
 		</div>
 	)
