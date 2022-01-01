8e36ca72da8d904ae6eeaab03c1a5edcbdd1ca81
Fury <ashiqasraf07@gmail.com>
2022-01-01 21:00:06 +0530

    Improved posts page

--- client/src/components/Post.jsx
+++ client/src/components/Post.jsx
@@ -4,20 +4,22 @@ const Post = ({ post }) => {
 	const PF = 'http://localhost:2506/images/'
 	return (
 		<div className="post">
-			{post.photo && <img src={PF + post.photo} alt="" className="postImg" />}
-			<div className="postInfo">
-				<div className="postCats">
+			{post.photo && <img src={PF + post.photo} alt="Post" className="post__img" />}
+			<div className="post__info">
+				<div className="post__info--cats">
 					{post?.categories.map(cat => (
-						<span className="postCat">{cat}</span>
+						<span className="post__info--cat" key={cat}>
+							{cat}
+						</span>
 					))}
 				</div>
 				<Link to={`/post/${post._id}`} className="link">
-					<span className="postTitle">{post.title}</span>
+					<span className="post__title">{post.title}</span>
 				</Link>
 				<hr />
-				<span className="postDate">{new Date(post.createdAt).toDateString()}</span>
+				<span className="post__date">{new Date(post.createdAt).toDateString()}</span>
 			</div>
-			<p className="postDesc">{post.desc}</p>
+			<p className="post__desc">{post.desc}</p>
 		</div>
 	)
 }
