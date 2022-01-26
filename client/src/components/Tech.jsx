bc21441723c65f544074db62cea8a233e4d449b5
Fury <ashiqasraf07@gmail.com>
2022-01-26 23:25:46 +0530

    Animated about page

--- client/src/components/Tech.jsx
+++ client/src/components/Tech.jsx
@@ -1,4 +1,24 @@
+import gsap from 'gsap'
+import { useEffect } from 'react'
+
 const Tech = ({ img, name, desc, alt }) => {
+	useEffect(() => {
+		gsap.fromTo(
+			'.about__tech',
+			{
+				y: '-30',
+				duration: 1,
+				ease: 'ease-out',
+				opacity: 0,
+			},
+			{
+				y: 0,
+				delay: 2,
+				opacity: 1,
+			}
+		)
+	})
+
 	return (
 		<div className={`about__tech about__tech--${alt}`}>
 			<img src={img} alt={alt} className="about__tech--img" />
